75425eb36d42316c844a1a7536b089c1d02449b7
KevinQbds11 <kcastillo93@uan.edu.co>
2022-09-17 19:41:13 -0500

    Diseño inicial

--- app/src/main/java/com/example/randomsalas/MainActivity.kt
+++ app/src/main/java/com/example/randomsalas/MainActivity.kt
@@ -8,4 +8,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun main(args: Array<String>) {
+        println("Hola mundo")
+        val edadFirulais: Int = 2
+        val edadFirulais2: Int = 3
+        println("La edad de firulais es: $edadFirulais:Int y el tiempo que le queda de vida es: $edadFirulais2")
+        val myVariable: Int = edadFirulais
+        val nombre: String = "Juan"
+        var myArray: String = "Mi nombre es $nombre y tiene ${nombre.length} caracteres"
+        println(myArray[0])
+
+    }
 }
